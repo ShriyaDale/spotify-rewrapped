8777cce06157d438767421e4c800bad2ddbefa15
Shriya Dale <shriyadale85@gmail.com>
2026-02-21 10:44:35 -0500

    initial template

---
+++ src/app/layout.tsx
@@ -0,0 +1,18 @@
+import type { Metadata } from 'next';
+import './globals.css';
+
+export const metadata: Metadata = {
+  title: 'Spotify Rewrapped',
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+      </head>
+      <body>{children}</body>
+    </html>
+  );
+}
